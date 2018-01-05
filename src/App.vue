9430fa6cb19823636093466aaf1852d374969b08
Caleb Hoover <choover11@gmail.com>
2018-01-05 13:33:53 -0800

    mdl!

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
-  <div id="app">
-    <button @click="login">Login</button>
-    <button @click="logout">Logout</button>
-    <button @click="fetchData">Fetch</button>
-    <ul v-if="competitions">
-      <li v-for="(comp,index) in competitions" :key="index">{{comp.name}}</li>
-    </ul>
-    <router-view/>
+  <div id="app" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Groups</span>
+        <div class="mdl-layout-spacer"></div>
+
+        <div class="mdl-navigation">
+          <button v-if="isLoggedIn" @click="logout" href="/" class="mdl-button" style="color: white">Logout</button>
+          <button v-else @click="login" href="/" class="mdl-button" style="color: white">Login</button>
+
+          <button v-if="isLoggedIn" id="demo-menu-lower-right" class="mdl-button mdl-js-button mdl-button--icon">
+            <i class="material-icons">more_vert</i>
+          </button>
+
+          <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="demo-menu-lower-right">
+            <li class="mdl-menu__item" @click="fetchData">Refresh</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <ul class="mdl-list">
+        <li v-for="(comp,index) in competitions" :key="index" class="mdl-list__item">{{comp.name}}</li>
+      </ul>
+      <router-view/>
+    </main>
   </div>
 </template>
 
@@ -20,11 +38,14 @@ export default {
     }
   },
   created () {
-    this.fetchData()
+    if (this.$store.getters.isLoggedIn) {
+      this.fetchData()
+    }
   },
   computed: {
     ...mapGetters({
-      competitions: 'getCompetitions'
+      competitions: 'getCompetitions',
+      isLoggedIn: 'isLoggedIn'
     })
   },
   methods: {
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import qs from 'qs'
-import {gotoPreLoginPath} from '@/wcaApi'
 import Home from '@/components/Home'
 
 Vue.use(Router)
-
-const getHashParameter = name => qs.parse(window.location.hash.slice(1))[name]
-
-let wcaAccessToken = getHashParameter('access_token')
-if (wcaAccessToken) {
-  window.location.has = ''
-  localStorage['groups.accessToken'] = wcaAccessToken
-  gotoPreLoginPath()
-}
 
 export default new Router({
   mode: 'history',
