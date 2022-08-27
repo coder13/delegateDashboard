e7408ccc4fae6cbed2c619a1abb7dc275e117ef8
Choover <choover11@gmail.com>
2022-08-27 13:18:38 -0700

    Added switch to show all rounds

--- src/components/pages/Competition/RoundSelector.js
+++ src/components/pages/Competition/RoundSelector.js
@@ -8,12 +8,13 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import { makeStyles } from '@mui/styles';
-import { activityById, groupActivitiesByRound } from '../../../lib/activities';
+import { activityById, groupActivitiesByRound, parseActivityCode } from '../../../lib/activities';
 import { eventNameById } from '../../../lib/events';
 import { personsShouldBeInRound } from '../../../lib/persons';
 import { pluralize } from '../../../lib/utils';
 import { getExtensionData } from '../../../lib/wcif-extensions';
 import { useBreadcrumbs } from '../../providers/BreadcrumbsProvider';
+import { Divider, FormControlLabel, Switch } from '@mui/material';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,6 +44,8 @@ const RoundSelectorPage = () => {
   const wcif = useSelector((state) => state.wcif);
   const classes = useStyles();
 
+  const [showAllRounds, setShowAllRounds] = React.useState(false);
+
   useEffect(() => {
     setBreadcrumbs([
       {
@@ -52,59 +55,74 @@ const RoundSelectorPage = () => {
   }, [setBreadcrumbs]);
 
   return (
-    <List className={classes.root}>
-      {wcif.events.map((event) => (
-        <li key={event.id} className={classes.listSection}>
-          <ul className={classes.ul}>
-            <ListSubheader>{eventNameById(event.id)}</ListSubheader>
-            {event.rounds.map((round, index) => {
-              const realGroups = groupActivitiesByRound(wcif, round.id);
-              const groupsData = getExtensionData('groups', round);
-
-              const _personsShouldBeInRound = personsShouldBeInRound(wcif, round)?.length;
-
-              const personsAssigned = wcif.persons.filter((p) =>
-                p.assignments.find((a) => {
-                  const activity = activityById(wcif, a.activityId);
-                  return (
-                    activity.activityCode.split('-')[0] === round.id.split('-')[0] &&
-                    activity.activityCode.split('-')[1] === round.id.split('-')[1]
-                  );
+    <>
+      <FormControlLabel
+        control={<Switch />}
+        label={'Show All Rounds'}
+        checked={showAllRounds}
+        onChange={(event) => setShowAllRounds(event.target.checked)}
+        inputProps={{ 'aria-label': 'controlled' }}
+      />
+      <Divider />
+      <List className={classes.root}>
+        {wcif.events.map((event) => (
+          <li key={event.id} className={classes.listSection}>
+            <ul className={classes.ul}>
+              {showAllRounds && <ListSubheader>{eventNameById(event.id)}</ListSubheader>}
+              {event.rounds
+                .filter((round) => {
+                  const { roundNumber } = parseActivityCode(round.id);
+                  return roundNumber === 1 || showAllRounds;
                 })
-              ).length;
+                .map((round, index) => {
+                  const realGroups = groupActivitiesByRound(wcif, round.id);
+                  const groupsData = getExtensionData('groups', round);
 
-              const realGroupsGeneratedText = realGroups?.length && `${pluralize(realGroups.length, 'group', 'groups')} generated`;
-              const configuredGroupsText = groupsData?.groups
-                ? `${pluralize(groupsData?.groups, 'group', 'groups')} configured`
-                : 'No Groups Configured';
+                  const _personsShouldBeInRound = personsShouldBeInRound(wcif, round)?.length;
+
+                  const personsAssigned = wcif.persons.filter((p) =>
+                    p.assignments.find((a) => {
+                      const activity = activityById(wcif, a.activityId);
+                      return (
+                        activity.activityCode.split('-')[0] === round.id.split('-')[0] &&
+                        activity.activityCode.split('-')[1] === round.id.split('-')[1]
+                      );
+                    })
+                  ).length;
+
+                  const realGroupsGeneratedText = realGroups?.length && `${pluralize(realGroups.length, 'group', 'groups')} generated`;
+                  const configuredGroupsText = groupsData?.groups
+                    ? `${pluralize(groupsData?.groups, 'group', 'groups')} configured`
+                    : 'No Groups Configured';
 
 
-              const textToShow = [
-                realGroups?.length ? realGroupsGeneratedText : configuredGroupsText,
-                `${pluralize(personsAssigned, 'person', 'people')} assigned of ${_personsShouldBeInRound || '???'
-                }`,
-              ];
+                  const textToShow = [
+                    realGroups?.length ? realGroupsGeneratedText : configuredGroupsText,
+                    `${pluralize(personsAssigned, 'person', 'people')} assigned of ${_personsShouldBeInRound || '???'
+                    }`,
+                  ];
 
-              return (
-                <ListItem
-                  key={round.id}
-                  button
-                  component={RouterLink}
-                  to={`/competitions/${competitionId}/events/${round.id}`}>
-                  <ListItemAvatar>
-                    <span className={`cubing-icon event-${event.id}`} />
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={`${eventNameById(event.id)} Round ${index + 1}`}
-                    secondary={textToShow.join(' | ')}
-                  />
-                </ListItem>
-              );
-            })}
-          </ul>
-        </li>
-      ))}
-    </List>
+                  return (
+                    <ListItem
+                      key={round.id}
+                      button
+                      component={RouterLink}
+                      to={`/competitions/${competitionId}/events/${round.id}`}>
+                      <ListItemAvatar>
+                        <span className={`cubing-icon event-${event.id}`} />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={`${eventNameById(event.id)} Round ${index + 1}`}
+                        secondary={textToShow.join(' | ')}
+                      />
+                    </ListItem>
+                  );
+                })}
+            </ul>
+          </li>
+        ))}
+      </List>
+    </>
   );
 };
 
