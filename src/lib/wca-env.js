6d7828cd414572ab0cc7d51ec567b9270dc513a4
Choover <choover11@gmail.com>
2022-08-28 11:43:40 -0700

    refactored env variable handling

--- src/lib/wca-env.js
+++ src/lib/wca-env.js
@@ -1,11 +1,9 @@
 console.log(process.env)
 const searchParams = new URLSearchParams(window.location.search);
-export const PRODUCTION = process.env.NODE_ENV === 'production' && !searchParams.has('staging');
+export const STAGING_QUERY_PARAMS = !searchParams.has('staging');
 
-export const WCA_ORIGIN = PRODUCTION
-  ? 'https://www.worldcubeassociation.org'
-  : process.env.REACT_APP_DEV_WCA_OAUTH_WCA_ORIGIN || 'https://staging.worldcubeassociation.org';
+const STAGING_URL = 'https://staging.worldcubeassociation.org';
+const STAGING_APP_ID = 'example-application-id';
 
-export const WCA_OAUTH_CLIENT_ID = PRODUCTION
-  ? process.env.REACT_APP_WCA_OAUTH_CLIENT_ID
-  : process.env.REACT_APP_DEV_WCA_OAUTH_CLIENT_ID || 'example-application-id';
+export const WCA_ORIGIN = STAGING_QUERY_PARAMS ? STAGING_URL : (process.env.REACT_APP_WCA_ORIGIN || STAGING_URL)
+export const WCA_OAUTH_CLIENT_ID = STAGING_QUERY_PARAMS ? STAGING_APP_ID : (process.env.REACT_APP_WCA_OAUTH_CLIENT_ID || STAGING_APP_ID)
