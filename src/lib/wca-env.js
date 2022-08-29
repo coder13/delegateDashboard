9b2b86ea2e4d29b04d7d2d960bf1e4d62e701f08
Choover <choover11@gmail.com>
2022-08-29 11:37:47 -0700

    ugh

--- src/lib/wca-env.js
+++ src/lib/wca-env.js
@@ -1,6 +1,6 @@
 console.log(process.env);
 const searchParams = new URLSearchParams(window.location.search);
-export const STAGING_QUERY_PARAMS = !searchParams.has('staging');
+export const STAGING_QUERY_PARAMS = searchParams.has('staging');
 
 const STAGING_URL = 'https://staging.worldcubeassociation.org';
 const STAGING_APP_ID = 'example-application-id';
