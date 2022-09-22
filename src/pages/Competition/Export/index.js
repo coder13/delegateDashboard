b2098e7c260a825aa9cab09e231d828f94f4c70e
Caleb <choover11@gmail.com>
2022-09-21 19:06:33 -0700

    Updated export to export comp name, not id

--- src/pages/Competition/Export/index.js
+++ src/pages/Competition/Export/index.js
@@ -212,7 +212,7 @@ const ExportPage = () => {
           people.forEach((person) => {
             scorecards.push({
               id: person.registrantId,
-              competition_name: wcif.id,
+              competition_name: wcif.name,
               event_name: eventNameById(event.id),
               round_number: parseActivityCode(round.id)?.roundNumber,
               group_name: competingAssignmentToText(groupActivity),
