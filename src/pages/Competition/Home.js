a4554b586a875e3f282d74b19d731290fc3f6f5d
Choover <choover11@gmail.com>
2022-10-13 13:09:24 -0700

    finished unfinished refactoring

--- src/pages/Competition/Home.js
+++ src/pages/Competition/Home.js
@@ -1,47 +1,9 @@
-import { intlFormat } from 'date-fns';
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Card, Grid, Typography } from '@mui/material';
-import { CardContent } from '@mui/material';
+import { Grid } from '@mui/material';
+import CompetitionSummary from '../../components/CompetitionSummaryCard';
 import RoundSelectorPage from '../../components/RoundSelector';
-import { shortEventNameById } from '../../lib/events';
-import { acceptedRegistrations } from '../../lib/persons';
-import { pluralize } from '../../lib/utils';
 import { useBreadcrumbs } from '../../providers/BreadcrumbsProvider';
-
-const CompetitionSummary = () => {
-  const wcif = useSelector((state) => state.wcif);
-  const approvedRegistrations = acceptedRegistrations(wcif.persons);
-
-  return (
-    <Card>
-      <CardContent>
-        <Typography variant="h3" paragraph>
-          {wcif.name}
-        </Typography>
-        <Typography>
-          <b>Date: </b>
-          {intlFormat(new Date(wcif.schedule.startDate), {
-            weekday: 'short',
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-          })}{' '}
-          ({pluralize(wcif.schedule.numberOfDays, 'day', 'days')})
-        </Typography>
-        <Typography>
-          <b>Competitors: </b>
-          {approvedRegistrations.length}
-        </Typography>
-        <Typography>
-          <b>Events: </b>
-          {wcif.events.map((event) => shortEventNameById(event.id)).join(', ')}
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-};
 
 const CompetitionHome = () => {
   const { setBreadcrumbs } = useBreadcrumbs();
