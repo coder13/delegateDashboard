059d8a9b96c29e0391714e10748eaaacca6ea3ab
Choover <choover11@gmail.com>
2023-02-04 06:54:02 -0800

    Fixed generating competing assignments from staff

--- src/lib/groupAssignments/helpers.ts
+++ src/lib/groupAssignments/helpers.ts
@@ -1,6 +1,11 @@
 import { Activity, Person } from '@wca/helpers';
-import { ActivityWithParent, byGroupNumber, parseActivityCode } from '../activities';
+import { ActivityWithParent, parseActivityCode } from '../activities';
 import { isStaffAssignment } from '../assignments';
+
+const groupNumberFromActivityCode = (activityCode: string) => {
+  const { groupNumber } = parseActivityCode(activityCode);
+  return groupNumber as number;
+};
 
 /**
  * Determines the soonest group that this person is assigned
@@ -11,17 +16,34 @@ export const getSoonestAvailableActivity = (groups: ActivityWithParent[]) => (pe
   const assignedStaffAssignments = person.assignments?.filter(isStaffAssignment) || []; // By definition, should have at least one
   const assignedStaffActivities = assignedStaffAssignments
     .map(({ activityId }) => groups.find((g) => g.id === +activityId))
-    .filter(Boolean) as Activity[];
+    .filter(Boolean) as Activity[] & { at: (index: number) => Activity };
 
-  // Filter to groups where the person does not have a staff assignment for the given group number
-  return groups
-    .filter(
-      (g) =>
-        !assignedStaffActivities.some(
-          (a) =>
-            parseActivityCode(g.activityCode).groupNumber ===
-            parseActivityCode(a.activityCode).groupNumber
-        )
-    )
-    .sort(byGroupNumber)[0];
+  const assignedInGroup = new Map<number, Activity>();
+  assignedStaffActivities.forEach((a) => {
+    const groupNumber = groupNumberFromActivityCode(a.activityCode);
+    assignedInGroup.set(groupNumber, a);
+  });
+
+  // 1 based number incrementor
+  const inc = (x: number) => ((x - 1 - 1 + groups.length) % groups.length) + 1;
+  const lastGroupNumber = Math.max(
+    ...assignedStaffActivities.map((a) => groupNumberFromActivityCode(a.activityCode))
+  );
+
+  let groupNumber = inc(lastGroupNumber);
+  let groupsTried = 1;
+  while (groupsTried < groups.length) {
+    // if they are already assigned
+    if (!assignedInGroup.has(groupNumber)) {
+      break;
+    }
+    groupNumber = inc(groupNumber);
+    groupsTried++;
+  }
+
+  if (groupsTried === groups.length) {
+    return null;
+  }
+
+  return groups.find((g) => groupNumberFromActivityCode(g.activityCode) === groupNumber);
 };
