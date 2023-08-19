3a0ddc1818516d712c3c876d540120561d961f42
Choover <choover11@gmail.com>
2023-08-19 03:38:27 -0700

    WIP: wca-group-generator testing

--- src/store/reducers/generateAssignments.ts
+++ src/store/reducers/generateAssignments.ts
@@ -1,26 +1,6 @@
-import { Activity, Competition, Person, parseActivityCode } from '@wca/helpers';
-import { Constraints, assignCluster } from 'wca-group-generators';
-import { activitiesOverlap, activityCodeIsChild, findAllActivities } from '../../lib/activities';
-
-const createAcceptedPersonsCluster = (persons: Person[]) => {
-  return persons.filter((p) => p.registration?.status === 'accepted');
-};
-
-const createPersonsInRoundCluster = (wcif: Competition, roundId: string) => (persons: Person[]) => {
-  const { eventId, roundNumber } = parseActivityCode(roundId);
-
-  if (roundNumber === 1) {
-    return persons.filter((p) => p.registration?.eventIds.includes(eventId));
-  }
-
-  const round = wcif.events
-    .find((event) => event.id === eventId)
-    ?.rounds?.find((round) => round.id === roundId);
-
-  return persons.filter((person) =>
-    round?.results.find((result) => result.personId === person.registrantId)
-  );
-};
+import { Competition, parseActivityCode } from '@wca/helpers';
+import { applySteps, Steps } from 'wca-group-generators';
+import { activityCodeIsChild, findAllActivities } from '../../lib/activities';
 
 /**
  * Fills in assignment gaps. Everyone should end up having a competitor assignment and staff assignment
@@ -37,85 +17,6 @@ export function generateAssignments(
   },
   action
 ) {
-  // const activityCodes = [...new Set(activities.map((a) => a.activityCode))].sort();
-  // const lastActivityCode = activityCodes[activityCodes.length - 1];
-
-  // const groupsForFirstTimers = activities.filter((i) => i.activityCode !== lastActivityCode);
-
-  // const groupGenerator = new GroupGenerator(state.wcif as Competition);
-
-  // debugger;
-  // groupGenerator.addConstraint('competitor', Constraints.createUniqueAssignmentConstraint);
-  // groupGenerator.addConstraint('competitor', Constraints.mustBeInRoundConstraint);
-  // groupGenerator.addConstraint('competitor', Constraints.balancedGroupSize);
-  // groupGenerator.addConstraint(
-  //   'competitor',
-  //   Constraints.SpreadfirstTimersAcrossGroups(groupsForFirstTimers.map((i) => i.activityCode)),
-  //   10
-  // );
-
-  // groupGenerator.addConstraint('staff-judge', Constraints.mustBeInRoundConstraint);
-  // groupGenerator.addConstraint('staff-judge', Constraints.mustNotHaveOtherAssignmentsConstraint);
-  // groupGenerator.addConstraint(
-  //   'staff-judge',
-  //   createConstraint(
-  //     'delegateDashboard_judge_assignment_follows_competitor_assignment',
-  //     (_, activity, __, person) => {
-  //       if (!person?.assignments) {
-  //         return 0;
-  //       }
-
-  //       const competitorAssignment = person?.assignments.find(
-  //         (a) => a.assignmentCode === 'competitor'
-  //       );
-  //       if (!competitorAssignment) {
-  //         return 0;
-  //       }
-
-  //       const groupNumbers = new Set(
-  //         activities.map((a) => parseActivityCode(a.activityCode).groupNumber) as number[]
-  //       );
-  //       const competitorActivity = activities.find((a) => a.id === competitorAssignment.activityId);
-  //       if (!competitorActivity) {
-  //         return 0;
-  //       }
-
-  //       const competitorRoomId = findRooms(state.wcif).find((room) => {
-  //         const childActivities = room.activities.flatMap((a) => a.childActivities);
-  //         return childActivities.some((ca) => ca.id === competitorActivity.id);
-  //       })?.id;
-  //       const roomId = findRooms(state.wcif).find((room) => {
-  //         const childActivities = room.activities.flatMap((a) => a.childActivities);
-  //         return childActivities.some((ca) => ca.id === activity.id);
-  //       })?.id;
-
-  //       const { groupNumber } = parseActivityCode(activity.activityCode);
-  //       const { groupNumber: competitorActivityGroupNumber } = parseActivityCode(
-  //         competitorActivity.activityCode
-  //       );
-
-  //       if (!competitorActivityGroupNumber || !groupNumber || !competitorRoomId || !roomId) {
-  //         return 0;
-  //       }
-
-  //       const roomIdScore = competitorRoomId === roomId ? 1000 : 0;
-  //       if (groupNumber === (competitorActivityGroupNumber % groupNumbers.size) + 1) {
-  //         return 5000 + roomIdScore;
-  //       }
-
-  //       return 0;
-  //     }
-  //   )
-  // );
-
-  // groupGenerator.generate(['competitor', 'staff-judge'], activities);\
-
-  // state.wcif
-
-  const acceptedPersons = createAcceptedPersonsCluster(state.wcif.persons);
-  const personsInRound = createPersonsInRoundCluster(state.wcif, action.roundId)(acceptedPersons);
-  console.log(personsInRound);
-
   // Validate scramblers
 
   // validate runners
@@ -133,115 +34,47 @@ export function generateAssignments(
     )
     .sort((a, b) => a.activityCode.localeCompare(b.activityCode));
 
-  // const activityCodes = [...new Set(activities.map((a) => a.activityCode))].sort();
-  // const lastActivityCode = activityCodes[activityCodes.length - 1];
+  const activityCodes = [...new Set(activities.map((a) => a.activityCode))].sort();
+  const lastActivityCode = activityCodes[activityCodes.length - 1];
+  const lastGroupNumber = parseActivityCode(lastActivityCode).groupNumber as number;
 
-  const staffWithoutCompetitorAssignments = personsInRound.filter(
-    (p) =>
-      // has staff assignment
-      p.assignments?.some(
-        (assignment) =>
-          assignment.assignmentCode.includes('staff') &&
-          activities.some((a) => a.id === assignment.activityId)
-      ) &&
-      // but does not have competitor assignment
-      !p.assignments?.some(
-        (assignment) =>
-          assignment.assignmentCode === 'competitor' &&
-          activities.some((a) => a.id === assignment.activityId)
-      )
-  );
-  const allActivities = findAllActivities(state.wcif);
-  // const groupNumbers = new Set(
-  //   activities.map((a) => parseActivityCode(a.activityCode).groupNumber) as number[]
-  // );
+  const firstTimerActivities = activities.filter((a) => {
+    const { groupNumber } = parseActivityCode(a.activityCode);
+    return groupNumber !== lastGroupNumber;
+  });
 
-  // for each staff, figure out the best competitor assignment for them.
-  const transformedPersons = assignCluster({
-    cluster: staffWithoutCompetitorAssignments,
+  const wcif = applySteps({
     wcif: state.wcif,
-    activities,
-    assignmentCode: 'competitor',
-    constraints: [
+    steps: [
       {
-        constraint: Constraints.createUniqueAssignmentConstraint,
-        weight: 1,
+        step: Steps.generateCompetitorAssignments,
+        activities,
+        options: {},
       },
       {
-        constraint: {
-          name: 'must_not_have_conflicting_assignments',
-          // returns null if the person has an assignment at the same or overlapping times
-          score: (_, activity, __, person) => {
-            return person.assignments?.some((assignment) => {
-              const assignedActivity = allActivities.find((a) => a.id === assignment.activityId);
-
-              if (!assignedActivity) {
-                return false;
-              }
-
-              return activitiesOverlap(activity, assignedActivity);
-            })
-              ? null
-              : 0;
+        step: Steps.generateCompetitorAssignments,
+        activities: firstTimerActivities,
+        options: {
+          clusterOptions: {
+            firstTimerActivities: true,
           },
         },
-        weight: 1,
       },
       {
-        constraint: {
-          name: 'maximize_distance_between_staff_and_competing_assignment',
-          score: (_, activity, __, person) => {
-            if (!person?.assignments) {
-              return 0;
-            }
-
-            const staffAssignments = person?.assignments
-              .filter(
-                (assignment) =>
-                  assignment.assignmentCode.includes('staff') &&
-                  activities.find((a) => a.id === assignment.activityId)
-              )
-              .map((assignment) => {
-                // Implication from the filter means that activity is not undefined
-                const _activity = allActivities.find(
-                  (a) => a.id === assignment.activityId
-                ) as Activity;
-
-                return {
-                  ...assignment,
-                  activity: _activity,
-                  groupNumber: parseActivityCode(_activity.activityCode).groupNumber,
-                };
-              })
-              .sort((a, b) => a.activity.startTime.localeCompare(b.activity.startTime));
-
-            // const { groupNumber } = parseActivityCode(activity.activityCode);
-            if (activity.startTime < staffAssignments[0].activity.startTime) {
-              return 1;
-            }
-
-            // if not maximize the number of breaks before.
-
-            console.log(166, person.name, staffAssignments);
-
-            // const competitorRoomId = findRooms(state.wcif).find((room) => {
-            //   const childActivities = room.activities.flatMap((a) => a.childActivities);
-            //   return childActivities.some((ca) => ca.id === competitorActivity.id);
-            // })?.id;
-
-            return 0;
-          },
-        },
-        weight: 10,
+        step: Steps.generateCompetitorAssignments,
+        activities,
+        options: {},
+      },
+      {
+        step: Steps.GenerateJudgeAssignmentsForCompetitors,
+        activities,
+        options: {},
       },
     ],
   });
 
   return {
     ...state,
-    wcif: {
-      ...state.wcif,
-      persons: transformedPersons,
-    },
+    wcif,
   };
 }
