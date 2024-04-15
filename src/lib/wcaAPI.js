5823d76e090ea9ac2644e5262a8d0c566cb21a7f
Choover <choover11@gmail.com>
2024-04-15 12:48:13 -0700

    Sort by negative start date to get most recent comps first

--- src/lib/wcaAPI.js
+++ src/lib/wcaAPI.js
@@ -21,6 +21,7 @@ export const getUpcomingManageableCompetitions = () => {
   const params = new URLSearchParams({
     managed_by_me: true,
     start: oneWeekAgo.toISOString(),
+    sort: '-start_date',
   });
   return wcaApiFetch(`/competitions?${params.toString()}`);
 };
